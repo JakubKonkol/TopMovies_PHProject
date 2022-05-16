f4b8b0f948a6a95fd717ace0c21b013093505bf4
jakubkonkol <s24406@pjwstk.edu.pl>
2022-05-16 23:36:19 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title> TopMovies</title>
+    <link rel="stylesheet" href="css.css">
+</head>
+<body bgcolor="lightcyan">
+<div class="container">
+ <p class="title_text">TopMovies</p>
+<!-- TWORZY POLACZENIE Z BAZA DANYCH, SQL TO ZAPYTANIE A WHILE PRINTUJE PO KOLEI RZECZY Z TABELI WYBRANE SELECTEM -->
+<?php
+$polaczenie = mysqli_connect("127.0.0.1", "root", "", "TopMovies");
+$res = mysqli_query($polaczenie, "SELECT title, genre, quantity,price FROM filmy");
+while($r=mysqli_fetch_row($res)) {
+    echo "<div class='movies'><h3>Tytul: $r[0] <br> Gatunek: $r[1] <br> Ilośc w magazynie: $r[2] <br> Cena: $r[3] PLN<br></h3></div>";
+}
+?>
+<!-- ------------------------------------------------------------------------>
+
+</div>
+</body>
+
+</html>
+
